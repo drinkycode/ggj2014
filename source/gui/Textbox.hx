4d2890941d556043b1c43be9a0dca4a083a04d74
aeveis <aeveis@gmail.com>
2014-01-26 12:47:42 -0500

    other font

--- source/gui/Textbox.hx
+++ source/gui/Textbox.hx
@@ -13,18 +13,29 @@ import util.GAssets;
 class Textbox extends FlxSpriteGroup
 {
 
+	public static inline var DOG:Int = 0;
+	public static inline var HUMAN:Int = 1;
 	public var text:FlxText;
 	
 	private var _timer:Float = 0;
 	
-	public function new() 
+	public function new(type:Int = HUMAN) 
 	{
 		super();
 		
+		if(type == DOG) {
 		text = new FlxText(0, 270, FlxG.width, "");
-		text.setFormat(GAssets.getFile("awesome",GAssets.LOC_DATA,"ttf"), 24, 0xffffff, "center", FlxText.BORDER_OUTLINE, 0x000000, true);
-		add(text);
+		text.setFormat(GAssets.getFile("awesome", GAssets.LOC_DATA, "ttf"), 24, 0xffffff, "center", FlxText.BORDER_OUTLINE, 0x000000, true);
 		
+		} else {
+			
+			text = new FlxText(0, 270, FlxG.width, "");
+			text.setFormat(GAssets.getFile("HelvetiHand", GAssets.LOC_DATA, "ttf"), 24, 0xffffff, "center", FlxText.BORDER_OUTLINE, 0x000000, true);
+		
+		}
+		
+		
+		add(text);
 		scrollFactor.x = scrollFactor.y = 0;
 		
 		hide();
