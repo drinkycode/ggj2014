ac10d1a972595a530352117dc7f3e4488f5a1945
Michael Lee <nfreakct@gmail.com>
2014-01-25 20:45:50 -0500

    Fixed slippery controls

--- source/game/Player.hx
+++ source/game/Player.hx
@@ -9,7 +9,9 @@ import flixel.group.FlxSpriteGroup;
 import flixel.system.input.keyboard.FlxKey;
 import flixel.system.input.keyboard.FlxKeyboard;
 import flixel.system.input.keyboard.FlxKeyShortcuts;
+import flixel.util.FlxMath;
 import util.GAssets;
+import util.Util;
 
 /**
  * Doggy doge
@@ -38,28 +40,31 @@ class Player extends FlxGroup
 	{
 		super();
 		
-		sprite = new FlxSprite();
+		shadow = new FlxSprite();
+		shadow.makeGraphic(36, 14, 0xff000000);
+		shadow.alpha = 0.44;
 		
+		
+		sprite = new FlxSprite();
 		sprite.loadGraphic(GAssets.getFile("player"), true, true, 64, 64);
 		sprite.animation.add("idle", [0], 0, false);
 		sprite.animation.play("idle");
 		
 		sprite.width = 44;
-		sprite.height = 29;
-		//_sprite.resetSize();
-		//_sprite.resetSizeFromFrame();
+		sprite.height = 4;
 		sprite.centerOffsets();
-		sprite.offset.y += 4;
-		//_sprite.setOriginToCenter();
+		sprite.offset.y += 14;
+		sprite.moves = false;
+		
 		
 		interactionZone = new FlxSprite();
 		interactionZone.makeGraphic(24, 24, 0xffffffff);
 		interactionZone.alpha = 0.5;
-		//interactionZone.resetSize();
-		//interactionZone.centerOffsets();
-		//interactionZone.setOriginToCenter();
+		interactionZone.active = false;
 		interactionZone.moves = false;
 		
+		
+		add(shadow);
 		add(sprite);
 		add(interactionZone);
 		
@@ -135,34 +140,110 @@ class Player extends FlxGroup
 		{
 			case 0:
 				ix = 12;
-				iy = -16;
-			case 1:
-				ix = 40;
-				iy = -16;
+				iy = -40;
+			//case 1:
+				//ix = 40;
+				//iy = -16;
 			case 2:
 				ix = 40;
-				iy = 12;
-			case 3:
-				ix = 40;
-				iy = 40;
+				iy = -12;
+			//case 3:
+				//ix = 40;
+				//iy = 40;
 			case 4:
 				ix = 12;
-				iy = 40;
-			case 5:
-				ix = -16;
-				iy = 40;
+				iy = 16;
+			//case 5:
+				//ix = -16;
+				//iy = 40;
 			case 6:
 				ix = -16;
-				iy = 12;
-			case 7:
-				ix = -16;
-				iy = -16;
+				iy = -12;
+			//case 7:
+				//ix = -16;
+				//iy = -16;
 		}
 		
 		interactionZone.x = sprite.x + ix;
 		interactionZone.y = sprite.y + iy;
 		
 		super.update();
+		updateMotionExt();
+		
+		shadow.x = sprite.x + 4;
+		shadow.y = sprite.y + 2;
+	}
+	
+	private function updateMotionExt():Void
+	{
+		var delta:Float;
+		var velocityDelta:Float;
+		
+		var dt:Float = FlxG.elapsed;
+		
+		velocityDelta = 0.5 * (computeVelocity(sprite.velocity.x, sprite.acceleration.x, sprite.drag.x, sprite.maxVelocity.x) - sprite.velocity.x);
+		sprite.velocity.x += velocityDelta;
+		delta = sprite.velocity.x * dt;
+		sprite.velocity.x += velocityDelta;
+		sprite.x += delta;
+		
+		velocityDelta = 0.5 * (computeVelocity(sprite.velocity.y, sprite.acceleration.y, sprite.drag.y, sprite.maxVelocity.y) - sprite.velocity.y);
+		sprite.velocity.y += velocityDelta;
+		delta = sprite.velocity.y * dt;
+		sprite.velocity.y += velocityDelta;
+		sprite.y += delta;
+	}
+	
+	private function computeVelocity(Velocity:Float, Acceleration:Float, Drag:Float, Max:Float):Float
+	{
+		if (Acceleration != 0)
+		{
+			Velocity += Acceleration * FlxG.elapsed;
+			if (Util.sign(Acceleration) != Util.sign(Velocity))
+			{
+				var drag:Float = Drag * FlxG.elapsed;
+				if (Velocity - drag > 0)
+				{
+					Velocity = Velocity - drag;
+				}
+				else if (Velocity + drag < 0)
+				{
+					Velocity += drag;
+				}
+				else
+				{
+					Velocity = 0;
+				}
+			}
+		}
+		else if(Drag != 0)
+		{
+			var drag:Float = Drag * FlxG.elapsed;
+			if (Velocity - drag > 0)
+			{
+				Velocity = Velocity - drag;
+			}
+			else if (Velocity + drag < 0)
+			{
+				Velocity += drag;
+			}
+			else
+			{
+				Velocity = 0;
+			}
+		}
+		if((Velocity != 0) && (Max != 0))
+		{
+			if (Velocity > Max)
+			{
+				Velocity = Max;
+			}
+			else if (Velocity < -Max)
+			{
+				Velocity = -Max;
+			}
+		}
+		return Velocity;
 	}
 	
 	public function checkInteraction():Void
---
+++ source/util/Util.hx
@@ -0,0 +1,16 @@
+package util;
+
+/**
+ * Util functions
+ * 
+ * @author Michael Lee
+ */
+class Util
+{
+
+	public static function sign(N:Float):Int
+	{
+		return (N > 0) ? 1 : -1;
+	}
+	
+}
